01ddf85c4050382ee46fa286cc63f9a6b822da67
d.galich <d.galich@iva.ru>
2025-10-14 21:45:55 +0800

    langchain.agent

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from langchain_community.llms import LlamaCpp
+from langchain.agents import create_agent
 
-model_path = "D:\models\saiga_yandexgpt_8b.Q4_K_M.gguf"
+def main():
+    model_path = "D:\models\saiga_yandexgpt_8b.Q4_K_M.gguf"
+ 
+    llm = LlamaCpp(
+        model_path=model_path,
+        temperature=0.75,
+        max_tokens=2000,
+        n_ctx=4096,
+        verbose=True,
+    )
 
-llm = LlamaCpp(
-    model_path=model_path,
-    temperature=0.75,
-    max_tokens=2000,
-    n_ctx=4096,
-    verbose=True,
-)
+    agent = create_agent(llm)
 
-print(llm.invoke("What is the capital of France?"))
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
+
+        answer = agent.invoke({
+            "messages": [
+                {"role": "user", "content": user_input}
+            ]
+        })
+
+        print(answer)
+
+
+if __name__ == "__main__":
+    main()
