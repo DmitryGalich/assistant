c0eace5fa744929407d578efc3cfc157b34fb172
d.galich <d.galich@iva.ru>
2026-02-16 23:17:31 +0300

    understands difference between tools but imagins unexsisting tools

--- model_handler/main.py
+++ model_handler/main.py
@@ -6,15 +6,35 @@ from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
 
 class WeatherInput(BaseModel):
-    location: str = Field(description="The city and state, e.g. San Francisco, CA")
-    unit: str = Field(enum=["celsius", "fahrenheit"])
+    location: str = Field(description="Город или страна")
+    unit: str = Field(enum=["градусы Цельсия", "градусы Фаренгейта"])
 
 
 @tool("get_current_weather", args_schema=WeatherInput)
 def get_weather(location: str, unit: str):
-    """Get the current weather in a given location"""
-    return f"Now the weather in {location} is 999999 {unit}"
+    """Отвечать на вопрос какая погода сейчас в указанном месте"""
+    return f"Актуальная погода в {location} - это 999999 {unit}"
 
+
+# class WeatherInput(BaseModel):
+#     location: str = Field(description="The city and state, e.g. San Francisco, CA")
+#     unit: str = Field(enum=["celsius", "fahrenheit"])
+
+
+# @tool("get_current_weather", args_schema=WeatherInput)
+# def get_weather(location: str, unit: str):
+#     """Get the current weather in a given location"""
+#     return f"Now the weather in {location} is 999999 {unit}"
+
+# @tool("make_small_talk")
+# def get_weather():
+#     """Keep small talk"""
+#     return f"Making kek"
+
+@tool("make_small_talk")
+def make_small_talk():
+    """Поддержи разговор"""
+    return f"Making kek"
 
 def main():
     model_path = r"model_handler/models/meta-llama-3.1-8b-instruct-q6_k.gguf"
@@ -29,16 +49,27 @@ def main():
 
     agent = create_agent(
         model=model,
-        tools=[get_weather],
+        # tools=[get_weather, make_small_talk],
+        # system_prompt="you are smart assistant. Call tool if request about the weather. In other ways answer by simple text. Do NOT imagine tools"
+
     )
 
     input_object = {
         "messages": [
-            {"role": "user", "content": "какая погода в краснодаре?"}
+            # {"role": "user", "content": "what is the weather in Krasnodar?"}
+            # {"role": "user", "content": "what is up?"}
+
+            # {"role": "user", "content": "какая погода в краснодаре?"}
+            {"role": "user", "content": "как дела?"}
+
         ]
     }
     result = agent.invoke(input_object)
     print(result)
+    # messages = result["messages"]
+    # print(messages)
+    # last_response = messages[-1]
+    # print(last_response)
     print("===================================")
 
 if __name__ == "__main__":
