1a9c29722fc282df8b7817ecfe7f0e9ea079c715
d.galich <d.galich@iva.ru>
2026-03-01 23:10:08 +0300

    deep agent works but very slow. needs big context

--- model_handler/main.py
+++ model_handler/main.py
@@ -1,44 +1,41 @@
 from langchain_community.chat_models import ChatLlamaCpp
-from langchain_core.tools import tool
-from pydantic import BaseModel, Field
-from langchain.agents import create_agent
+# from langchain_core.tools import tool
+# from pydantic import BaseModel, Field
+# from langchain.agents import create_agent
 
-from langchain_core.messages import HumanMessage
-
+# from langchain_core.messages import HumanMessage
 
 from deepagents import create_deep_agent
 
-class WeatherInput(BaseModel):
-    location: str = Field(description="Город или страна")
-    unit: str = Field(enum=["градусы Цельсия", "градусы Фаренгейта"])
+# class WeatherInput(BaseModel):
+#     location: str = Field(description="Город или страна")
+#     unit: str = Field(enum=["градусы Цельсия", "градусы Фаренгейта"])
 
 
-@tool("get_current_weather", args_schema=WeatherInput)
-def get_weather(location: str, unit: str):
-    """Отвечать на вопрос какая погода сейчас в указанном месте"""
-    return f"Актуальная погода в {location} - это 999999 {unit}"
+# @tool("get_current_weather", args_schema=WeatherInput)
+# def get_weather(location: str, unit: str):
+#     """Отвечать на вопрос какая погода сейчас в указанном месте"""
+#     return f"Актуальная погода в {location} - это 999999 {unit}"
 
-@tool("make_small_talk")
-def make_small_talk():
-    """Поддержи разговор"""
-    return f"ИДИ НАХУЙ"
+# @tool("make_small_talk")
+# def make_small_talk():
+#     """Поддержи разговор"""
+#     return f"ИДИ НАХУЙ"
 
 def main():
-    kek= create_deep_agent()
-
-
     model_path = r"model_handler/models/meta-llama-3.1-8b-instruct-q6_k.gguf"
-
-    model = ChatLlamaCpp(
+    llm = ChatLlamaCpp(
         model_path=model_path,
         temperature=0,
         n_gpu_layers=-1,
         verbose=True,
         f16_kv=True,
+        n_ctx = 10000,
     )
 
-    agent = create_agent(
-        model=model,
+
+    agent= create_deep_agent(
+        model=llm
     )
 
     input_object = {
@@ -48,13 +45,12 @@ def main():
 
         ]
     }
+
     result = agent.invoke(input_object)
-    messages = result["messages"]
-    last_response = messages[-1]
+    print(result)
 
-    
-
-
+    # messages = result["messages"]
+    # last_response = messages[-1]
 
 if __name__ == "__main__":
     main()
